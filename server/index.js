e240624a79b20c106ccfba081540097bd4914aa4
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-08-12 07:21:02 +0530

    portset

--- server/index.js
+++ server/index.js
@@ -10,3 +10,25 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 
 
 app.use(cors())
+
+
+const CONNECTION_URL="mongodb+srv://mohammednihaljm:mohammednihaljm123@cluster0.xzsb5g9.mongodb.net/?retryWrites=true&w=majority"
+const PORT=process.env.PORT||5000;
+
+app.listen(PORT,()=>{
+    console.log(`App is listening port: ${PORT}`);
+})
+
+mongoose
+        .connect(CONNECTION_URL)
+        .then(()=>{
+            console.log("app connected to database");
+          
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+
+
+
+
