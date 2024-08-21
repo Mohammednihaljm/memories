32e3b418897b72d0ed2269ceaef9530a1be63fb0
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-08-21 23:03:13 +0530

    controllers

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,10 @@
 import express from "express"
+import {getPosts} from "../controllers/posts.js"
 
 
 const router=express.Router()
 
 
-router.get('/',(req,res)=>{
-    res.send("this work")
-})
+router.get('/',getPosts)
 
 export default router
