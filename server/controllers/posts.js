e2eb6cd4889c3f8dcf1d5df13a013838cb4fc12e
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-09-02 23:10:34 +0530

    createpost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,26 @@
-export const getPosts = (req, res) => {
-  res.send("this work");
+import postMessage from "../models/postMessage.js";
+
+
+export const getPosts = async(req, res) => {
+  try {
+    const postMessages= await postMessage.find()
+    // console.log(postMessages)
+    res.status(200).json(postMessages)
+  } catch (error) {
+    res.status(404).json({messages:error.message})
+  }
 };
+
+
+export const createPost=async(req,res)=>{
+  try {
+    // const body=req.body;
+    const post=req.body
+   const newpost= new postMessage(post)
+       await postMessage.save()
+       res.status(201).json(newpost)
+  } catch (error) {
+    res.status(404).json({messages:error.message})
+    
+  }
+}
